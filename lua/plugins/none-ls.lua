8b71fa8ebdd947d2a0fb374f9bcb52c923e9556d
GustavoCesarSantos <gustavocs789@gmail.com>
2024-10-21 13:01:07 -0300

    atualiza pacotes e configs

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,14 +4,27 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.codespell,
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.sql_formatter,
-				null_ls.builtins.diagnostics.eslint_d,
+				null_ls.builtins.completion.luasnip,
 				null_ls.builtins.diagnostics.sqlfluff.with({
 					extra_args = { "--dialect", "postgres" }, -- change to your dialect
 				}),
+                null_ls.builtins.diagnostics.buf,
+                null_ls.builtins.diagnostics.codespell,
+                null_ls.builtins.diagnostics.dotenv_linter,
+                null_ls.builtins.diagnostics.golangci_lint,
+				null_ls.builtins.formatting.codespell,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.sqlfluff.with({
+					extra_args = { "--dialect", "postgres" }, -- change to your dialect
+				}),
+                null_ls.builtins.formatting.sql_formatter,
+                null_ls.builtins.formatting.gofmt,
+                null_ls.builtins.formatting.goimports,
+                null_ls.builtins.formatting.golines,
+                null_ls.builtins.formatting.nginx_beautifier,
+                null_ls.builtins.formatting.protolint,
+                null_ls.builtins.formatting.yamlfix,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,21 @@ return {
 	config = function()
 		local config = require("nvim-treesitter.configs")
 		config.setup({
-      ensure_installed = { 'c', 'lua', 'javascript', 'typescript', 'go', 'rust' },
+			ensure_installed = {
+				"c",
+				"lua",
+				"javascript",
+				"typescript",
+				"go",
+				"rust",
+				"proto",
+				"sql",
+				"make",
+				"dockerfile",
+				"yaml",
+				"html",
+			},
+			sync_install = false,
 			auto_install = true,
 			highlight = { enable = true },
 			indent = { enable = true },
