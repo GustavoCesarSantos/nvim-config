f32035f0a4e24e0260a2a2b40f9ead1f772756f8
GustavoCesarSantos <gustavocs789@gmail.com>
2024-11-23 18:12:04 -0300

    wip: configurando plano de fundo transparente

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,6 +4,53 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
-        vim.cmd.colorscheme("catppuccin")
+        require("catppuccin").setup({
+      flavour = "mocha",
+      transparent_background = true,
+      styles = {
+        keywords = { "bold" },
+        functions = { "italic" },
+      },
+      custom_highlights = function(colors)
+        return {
+          -- custom
+          PanelHeading = {
+            fg = colors.lavender,
+            bg = colors.none,
+            style = { "bold", "italic" },
+          },
+
+          -- lazy.nvim
+          LazyH1 = {
+            bg = colors.none,
+            fg = colors.lavender,
+            style = { "bold" },
+          },
+          LazyButton = {
+            bg = colors.none,
+            fg = colors.subtext0,
+          },
+          LazyButtonActive = {
+            bg = colors.none,
+            fg = colors.lavender,
+            style = { "bold" },
+          },
+          LazySpecial = { fg = colors.green },
+
+          CmpItemMenu = { fg = colors.subtext1 },
+
+          FloatBorder = {
+            fg = colors.mantle,
+            bg = colors.none,
+          },
+
+          FloatTitle = {
+            fg = colors.base,
+            bg = colors.none,
+          },
+        }
+      end,
+    })
+    vim.cmd.colorscheme("catppuccin")
     end,
 }
